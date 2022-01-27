0108b35136785b987a6284b1e7bc3ad2f313859f
HUANG Manutea <haung.manutea@gmail.com>
2022-01-26 21:04:30 -1000

    fixed project name

--- frontend/basic-single-page/karma.conf.js
+++ frontend/basic-single-page/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
+      dir: require('path').join(__dirname, './coverage/basic-single-page'),
       subdir: '.',
       reporters: [
         { type: 'html' },
