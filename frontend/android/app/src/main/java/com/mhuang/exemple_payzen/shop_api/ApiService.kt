62a8f293600a6af6ac82b444b91907ea8ddca8cb
huang <huang.manutea@gmail.com>
2023-04-27 19:20:23 -1000

    fixed api

--- frontend/android/app/src/main/java/com/mhuang/exemple_payzen/shop_api/ApiService.kt
+++ frontend/android/app/src/main/java/com/mhuang/exemple_payzen/shop_api/ApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
-    @POST("/payment")
+    @POST("/rest/payment")
     suspend fun createPayment(@Body order: Order): Response<FormTokenResponse>
 }
